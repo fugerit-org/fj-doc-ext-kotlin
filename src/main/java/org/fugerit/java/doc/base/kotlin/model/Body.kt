ce83ada0f63d412ba38f9ebfeecfc3c2c1b3c428
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-05-06 21:33:50 +0200

    Kotlin configuration review

--- src/main/java/org/fugerit/java/doc/base/kotlin/model/Body.kt
+++
@@ -1,6 +0,0 @@
-package org.fugerit.java.doc.base.kotlin.model
-
-import org.fugerit.java.doc.base.model.DocContainer
-
-class Body : HelperDSL.BodyTag(DocContainer.TAG_NAME_BODY) {
-}
--- src/main/java/org/fugerit/java/doc/base/kotlin/model/Meta.kt
+++
@@ -1,6 +0,0 @@
-package org.fugerit.java.doc.base.kotlin.model
-
-import org.fugerit.java.doc.base.model.DocContainer
-
-class Meta : HelperDSL.TagWithText(DocContainer.TAG_NAME_META) {
-}
