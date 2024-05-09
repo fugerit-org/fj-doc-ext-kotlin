b22a4f58945e4e022bc66f9121cc6a01188b2fa9
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-05-09 10:27:46 +0200

    Removed POC based package org.fugerit.java.doc.base.kotlin.model

--- src/main/java/org/fugerit/java/doc/base/kotlin/model/Dsl.kt
+++
@@ -1,6 +0,0 @@
-@file:JvmName("DocKt")
-
-package org.fugerit.java.doc.base.kotlin.model
-
-fun doc(block: Doc.() -> Unit): Doc =
-    Doc().apply(block)
--- src/main/java/org/fugerit/java/doc/base/kotlin/model/Meta.kt
+++
@@ -1,7 +0,0 @@
-package org.fugerit.java.doc.base.kotlin.model
-
-import org.fugerit.java.doc.base.model.DocContainer
-
-class Meta : HelperDSL.TagWithText(DocContainer.TAG_NAME_META) {
-    fun info( name : String, value : String ) { addKid( Info(name, value) ) }
-}
